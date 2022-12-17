24e3836db228a3d637865c1f33455a97c59c7242
Станислав <sts55@mail.ru>
2022-12-17 18:42:11 +0300

    add rundom

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -1,3 +1,5 @@
-﻿double numberA = 7;
-double numberB = 2;
-Console.WriteLine(numberA/numberB);
+﻿double numberA = new Random().Next(1, 10); // 0 1 2 3 4 ... 9
+Console.WriteLine(numberA);
+double numberB = new Random().Next(1, 10); 
+Console.WriteLine(numberB);
+Console.WriteLine(numberA+numberB);
